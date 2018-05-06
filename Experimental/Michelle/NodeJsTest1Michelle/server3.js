6d4baa42d8dfa06e014f484e9824420cc18ffa92
Michelle Nie <2michellen@gmail.com>
2018-05-06 18:31:46 -0400

    renaming

---
+++ Experimental/Michelle/NodeJsTest1Michelle/server3.js
@@ -0,0 +1,109 @@
+const express = require('express');         // info: simplifies web app programming. 
+const appRoot = require('app-root-path');   // info: https://github.com/inxilpro/node-app-root-path
+const morgan = require('morgan');           // info: lib for http logging.
+const winston = require('winston');         // info: lib for app level logging. https://github.com/winstonjs/winston
+require('winston-daily-rotate-file');       // info: lib for log file handing.  https://github.com/winstonjs/winston-daily-rotate-file
+
+var portNum = 900; // The port 80 is already used for web browsing on your machine, pick another unique/unused port.
+var appName = "Web Server3";
+var noPageCaching = true; // During developmnet avoid page caching at the end-user/client machine
+
+var app = express();
+app.use(morgan('dev'));
+
+if (noPageCaching) {
+    app.disable('etag'); // Prevent web pages from being cached on the client machine
+}
+
+
+const fs = require('fs'); 
+const env = process.env.NODE_ENV || 'development'; // If the NODE_ENV environment variable is defined use that first. If not, default to development.
+const logDir = 'log'; 
+
+// Create the log directory if it does not exist 
+if (!fs.existsSync(logDir)) { fs.mkdirSync(logDir); } 
+// Use local time for log timestamp
+const tsFormat = () => (new Date()).toLocaleTimeString(); 
+
+
+// Define the custom settings for each logging transport
+var loggingOptions = {
+    rotatinglogFileConfig: {
+        level: env === 'development' ? 'verbose' : 'info', // Don't log verbose calls in Production mode
+        filename: `${appRoot}/log/trace-%DATE%.log`,
+        datePattern: 'YYYY-MM-DD-HH',
+        zippedArchive: true,
+        maxSize: '20m',
+        maxFiles: '14d' 
+    },
+    logFileConfig: {
+        level: env === 'development' ? 'verbose' : 'info',
+        filename: `${appRoot}/log/trace.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 2048,
+        maxFiles: 5,
+        colorize: false,
+    },
+    consoleConfig: {
+        level: 'verbose',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
+};
+
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console(loggingOptions.consoleConfig),
+        new winston.transports.DailyRotateFile(loggingOptions.rotatinglogFileConfig)
+        //new winston.transports.File(loggingOptions.logFileConfig)
+    ],
+    exitOnError: false, // do not exit on handled exceptions
+});
+
+
+
+// Intercept all requests to adjust headers
+app.use(function(req, res, next){
+    if (noPageCaching) {
+        // Add these 'no page caching' meta tags:
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.header("Pragma", "no-cache");
+        res.header("Expires", 0);  
+    } 
+    res.header("MyCustomHeader", "SomeValue");  
+
+    // Continue to next down stream route handler.
+    next();
+ });
+
+
+// Setup Routes:
+
+app.get('/', homePageHandler); // Show how you can call a separately defined function by name.
+
+function homePageHandler(req, res) {
+    var now = new Date();  
+    var msg = `HOME - Hello World from ${appName} [${now.toLocaleTimeString()}]`;
+    logger.info("Responding with: " + msg);
+    res.send(msg);
+}
+
+app.get('/About', function (req, res) {
+    logger.verbose("Returning about.html");
+    res.sendFile(`${appRoot}/about.html`);
+});
+
+
+app.get('/Spaces', function (req, res) {
+    logger.verbose("Returning spaces.json");
+    res.sendFile(`${appRoot}/data/spaces.json`);
+});
+
+
+
+var server = app.listen(portNum, function () {
+    var port = server.address().port
+    console.log("%s listening on port %s", appName, port)
+});
