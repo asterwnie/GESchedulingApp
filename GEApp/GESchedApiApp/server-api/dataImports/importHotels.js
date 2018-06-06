e65884483913e598b502ef323278a2aa490ed525
Jack N <jack888@msn.com>
2018-06-06 00:23:51 +0000

    import hotels from text-based data file

--- GEApp/GESchedApiApp/server-api/dataImports/importHotels.js
+++ GEApp/GESchedApiApp/server-api/dataImports/importHotels.js
@@ -19,7 +19,7 @@ var siteCode = appConfig.defaultSite; // The current default in HLS-MA in server
 // Note: currently in VS Code debug mode you have to rely on using the appConfig.defaultSite setting.
 if (args.length == 3 && args[2] != null) { siteCode = args[2]; }
 
-const fileName = `hotelsSheet-${siteCode}.json`;
+const fileName = `hotels-${siteCode}.txt`;
 
 let Hotel = getHotelType(siteCode);
 
@@ -27,46 +27,168 @@ var totalNumOfHotels = 0;
 var totalNumOfHotelsCreated = 0;
 
 const delyInSecs = 3;
-setInterval(() => doSheetImport(), delyInSecs * 1000); // Ensures db connection is established in getHotelType since it's an async operation.
+const timer = setInterval(() => doSheetImport(), delyInSecs * 1000); // Ensures db connection is established in getHotelType since it's an async operation.
 
 
 
 function doSheetImport() {
 
     try {
-        const jsonData = fs.readFileSync(`./server-api/dataImports/dataFiles/${fileName}`);
-        const hotelsFromSheet = JSON.parse(jsonData);
+        clearInterval(timer);
 
-        // Count the total first.
-        hotelsFromSheet.forEach((hotelFromSheet) => totalNumOfHotels += 1 );
-
-        hotelsFromSheet.forEach((hotelFromSheet) => createHotel(hotelFromSheet));
+        var fileData = fs.readFileSync(`./server-api/dataImports/dataFiles/${fileName}`).toString()
+        
+        var result = extractHotelItems(fileData);
+        if (result.success) {
+            console.log(`Total number of hotels parsed: ${result.hotels.length}`);
+            console.log(result.hotels);
+            result.hotels.forEach((hotel) => createHotel(hotel));
+        } else {
+            process.exit();
+        }
 
     } catch (err) {
         logger.error(`ADMIN: Error importing the Hotel collection into the database! Error: ${err}`);
         mongoose.disconnect();
-        return;
+        process.exit();
     }
 }
 
 
+function extractHotelItems(fileData) {
+    var result = null;
+    var hotelItems = [];
+    var newHotel = null;
+    var errorEncountered = false;
+    var currentItemSeq = 0;
 
-function createHotel(hotelFromSheet) {
+    fileData.split(/\r?\n/).every((line) => {
+
+        var directive = null;
+        var lineProcessed = false;
+
+        logger.info(`Processing line: ${line}`);
+
+        directive = "--hotel.Name:";
+        if (line.search(directive) > -1) {            
+            // Complete and store the previous pending hotel object if exist.
+            if (newHotel != null) {
+                var success = ValidateAndCollectHotel(newHotel, hotelItems);
+                if (!success) {
+                    errorEncountered = true;
+                    return false; // Return false to stop additional line processing.
+                }
+            }
+
+            var hotelName = line.replace(directive, "").trim();
+            // Start a new hotel instance to gather its properties.
+            if (hotelName != "") {
+
+                currentItemSeq += 1;
+
+                newHotel = { 
+                    seqNum: currentItemSeq,
+                    name: hotelName 
+                }
+            } else {
+                logger.error("ERROR: The hotel name is required!");
+                errorEncountered = true;
+                return false; // Return false to stop additional line processing.
+            }
+
+            lineProcessed = true;
+        }
+
+        directive = "--hotel.Address:"; // Note, there can be multiple address lines.
+        if (!lineProcessed && line.search(directive) > -1) {
+            var hotelAddressLine = line.replace(directive, "").trim();
+            if (hotelAddressLine != "") {
+                if (!newHotel.hasOwnProperty("address")) {
+                    newHotel.address = []; // Create the address line array.               
+                }
+                newHotel.address.push(hotelAddressLine);
+            }
+        }
+
+        directive = "--hotel.Phone:";
+        if (!lineProcessed && line.search(directive) > -1) {
+            var hotelPhone = line.replace(directive, "").trim();
+            if (hotelPhone != "") {
+                newHotel.phone = hotelPhone;
+            }
+        }
+
+        directive = "--hotel.Fax:";
+        if (!lineProcessed && line.search(directive) > -1) {
+            var hotelFax = line.replace(directive, "").trim();
+            if (hotelFax != "") {
+                newHotel.fax = hotelFax;
+            }
+        }
+
+        directive = "--hotel.CorpRates:";
+        if (!lineProcessed && line.search(directive) > -1) {
+            var hotelCorpRates = line.replace(directive, "").trim();
+            if (hotelCorpRates != "") {
+                newHotel.corpRates = hotelCorpRates;
+            }
+        }
+
+        return true; // Return true to continue processing for the next line item.
+    })
+
+    // Check to see if there's one last pending new one to be collected.
+    if (errorEncountered == false && newHotel != null) {
+        var success = ValidateAndCollectHotel(newHotel, hotelItems);
+        if (!success) {
+            errorEncountered = true;
+        }
+    }
+
+    result = {
+        success: true,
+        hotels: hotelItems
+    }
+
+    if (errorEncountered == true) {
+        result.success = false;
+    } 
+
+    return result;
+}
+
+
+function ValidateAndCollectHotel(newHotel, hotelItems) {
+    var valid = ValidateHotel(newHotel);
+    if (valid) {
+        hotelItems.push(newHotel);
+        totalNumOfHotels += 1;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function ValidateHotel(newHotel) {
+    if (!newHotel.hasOwnProperty("name") || newHotel.name == "") {
+        logger.error("ERROR: The hotel name is required!");
+        return false;
+    }
+    if (!newHotel.hasOwnProperty("address") || newHotel.address.length == 0) {
+        logger.error("ERROR: The hotel address is required!");
+        return false;
+    }
+    return true;
+}
+
+
+function createHotel(hotel) {
 
     try {
-        logger.info(`ADMIN: Adding hotel (${hotelFromSheet.Name}) to the database.`);
+        logger.info(`ADMIN: Adding hotel (${hotel.name}) to the database.`);
 
-        // The properies from the import spreedsheet does not match directly 
-        // so map them to a object that conforms to the Hotel schema.
-        var hotelInDBFormat = {
-            seqNum: hotelFromSheet.SeqNum,
-            name: hotelFromSheet.Name, 
-            address: hotelFromSheet.Address, 
-            phone: hotelFromSheet.Phone,
-            corporateRates: hotelFromSheet.CorporateRates
-        };
-
-        var newHotel = new Hotel(hotelInDBFormat);
+        var newHotel = new Hotel(hotel);
 
         var validationErr = newHotel.validateSync();
         if (validationErr != null) {
--- GEApp/GESchedApiApp/server-api/models/hotelModel.js
+++ GEApp/GESchedApiApp/server-api/models/hotelModel.js
@@ -16,7 +16,7 @@ const HotelSchema = new Schema({
     },
 
     address: {
-        type: String,
+        type: [String],
         required: [true, 'Hotel address is required!']
     },
 
@@ -24,7 +24,7 @@ const HotelSchema = new Schema({
         type: String
     },
 
-    corporateRates: {
+    corpRates: {
         type: String
     },
 
