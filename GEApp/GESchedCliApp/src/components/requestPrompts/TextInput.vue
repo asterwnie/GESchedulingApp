00f5a1721233a28eb0772fa219081465d0a9f2c9
Jack N <jack888@msn.com>
2018-06-28 03:44:24 +0000

    use orange color and thicker border for the admin comment box

--- GEApp/GESchedCliApp/src/components/requestPrompts/TextInput.vue
+++ GEApp/GESchedCliApp/src/components/requestPrompts/TextInput.vue
@@ -5,7 +5,7 @@
     <input type="text" :id="ctrlId" :screenNum="screenNum" class="is-request-data form-control form-control-sm">
     <p :id="dataInvalidMsgId" style="display:none;" class="text-danger">Invalid input.</p>
     <p :id="dataRequiredMsgId" style="display:none;" class="text-danger">This field is required.</p>
-    <textarea :id="adminCommentCtrlId" :screenNum="screenNum" style="display:none; border: 1px solid #c86888;" placeholder="Add Comment" class="is-request-data is-admin-comment form-control form-control-sm"></textarea>
+    <textarea :id="adminCommentCtrlId" :screenNum="screenNum" style="display:none; border: 2px solid orange;" placeholder="Add Comment" class="is-request-data is-admin-comment form-control form-control-sm"></textarea>
   </div>
 
 </template>
