800978db840df975d58b77d8f078da5ce0bd2bda
Jack N <jack888@msn.com>
2018-08-10 11:05:23 -0400

    In Admin Home, fixed typo to new use appConfig.requestStatusTagCancelled

--- GEApp/GESchedCliApp/src/components/admin/AdminHome.vue
+++ GEApp/GESchedCliApp/src/components/admin/AdminHome.vue
@@ -235,7 +235,7 @@ export default {
         },
 
         cancelledLabel() {
-            return this.$store.state.appConfig.requestStatusTagCanceled; 
+            return this.$store.state.appConfig.requestStatusTagCancelled; 
         },
 
 
