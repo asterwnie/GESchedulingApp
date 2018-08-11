e6f317402211a6f8e04dcb2e5b250fd627ef0e97
Jack N <jack888@msn.com>
2018-08-10 22:39:20 -0400

    SERVER OPERATOR MODIFICATION

--- GEApp/GESchedApiApp/fullRedeploy-HLS-MA.bat
+++ GEApp/GESchedApiApp/fullRedeploy-HLS-MA.bat
@@ -5,49 +5,65 @@ REM    + Get latest application files from GitHub
 REM    + Refresh the application reference data in the application database
 REM    + Uninstall and reinstall the application as a Windows service 
 
-set sourceDrive=C:
-set sourcePath=\GESchedulingApp\GESchedulingApp\GEApp\GESchedApiApp
-set targetPath=\GESchedulingApp\Backups\GESchedApiApp
 
-set sourceFolder=%sourceDrive%%sourcePath%
+set DRIVE=%1
+if "%1" == "" set DRIVE=C
+
+set sourceFolder=%DRIVE%:\GESchedulingApp\GESchedulingApp\GEApp\GESchedApiApp
+set backupRootFolder=%DRIVE%:\GESchedulingApp\Backups
+set backupFolder=%DRIVE%:\GESchedulingApp\Backups\GESchedApiApp
+
+set CurDate=%date:~10,4%-%date:~4,2%-%date:~7,2%
+set timestamp=%CurDate%-%time:~0,2%%time:~3,2%%time:~6,2%
+
+set backupFolder=%backupFolder%-%timestamp%
+
+echo Using source folder: %sourceFolder%
+echo Using backup folder: %backupFolder%
+
+
+call :Begin >fullRedeploy-HLS-MA-Log.txt 
+exit /b
+
+:Begin
+
+echo Timestamp: %timestamp%
+echo Using source folder: %sourceFolder%
+echo Using backup folder: %backupFolder%
+
+
 echo Check exist for: %sourceFolder%
 
 if not exist %sourceFolder% (
-    echo This directory does not exist: %sourceFolder%
-    set sourceDrive=D:
-    set sourceFolder=%sourceDrive%%sourcePath%
-    echo Check exist for: %sourceFolder%
-) 
 
-if not exist %sourceFolder% (
     echo This directory does not exist: %sourceFolder%
     echo Unable to find a source directory to do the backup! 
+    exit
+
 ) else (
 
-    set CurDate=%date:~10,4%%date:~4,2%%date:~7,2%
-    set timestamp=%CurDate%-%time:~0,2%%time:~3,2%%time:~6,2%
-
-    if not exist %sourceDrive%\GESchedulingApp\Backups (
-        mkdir %sourceDrive%\GESchedulingApp\Backups
+    echo Make sure backup root folder exist: %backupRootFolder%
+    if not exist %backupRootFolder% (
+        mkdir %backupRootFolder%
     )
 
-    set sourceBackupFolder=%sourceDrive%%targetPath%-%timestamp%
+    echo Creating backup folder: %backupFolder%
+    mkdir %backupFolder%
 
     echo Begin backing up file from %sourceFolder%
-    echo To directory %sourceBackupFolder%
-
-    mkdir %sourceBackupFolder%
 
     if ERRORLEVEL 1 (
         echo Unable to complete all operations!
+        exit
     )
 
-    echo Created backup directory: %sourceBackupFolder%
+    echo Created backup directory: %backupFolder%
 
-    xcopy /S /Y /I %sourceFolder%\*.* %sourceBackupFolder%
+    xcopy /S /Y /I %sourceFolder%\*.* %backupFolder%
     
     if ERRORLEVEL 1 (
         echo Unable to complete all operations!
+        exit
     )
 
     echo Stop the application Windows service.
@@ -73,6 +89,7 @@ if not exist %sourceFolder% (
 
     if ERRORLEVEL 1 (
         echo Unable to complete all operations!
+        exit
     )
 
     echo Completed getting the latest source files from GitHub
@@ -83,6 +100,7 @@ if not exist %sourceFolder% (
 
     if ERRORLEVEL 1 (
         echo Unable to complete all operations!
+        exit
     )
 
     echo Done re-import all application reference data.
@@ -93,6 +111,7 @@ if not exist %sourceFolder% (
 
     if ERRORLEVEL 1 (
         echo Unable to complete all operations!
+        exit
     )
 
     echo About to re-install the application's Windows service.
