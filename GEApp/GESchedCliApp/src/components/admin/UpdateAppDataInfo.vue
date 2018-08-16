b0802d7ba7ce089d19dab42fb8a1f9c88a75d6a7
Jack N <jack888@msn.com>
2018-08-15 22:58:21 -0400

    FInalize the text in the App Data Update card in the Admin Maintenance view

--- GEApp/GESchedCliApp/src/components/admin/UpdateAppDataInfo.vue
+++ GEApp/GESchedCliApp/src/components/admin/UpdateAppDataInfo.vue
@@ -3,10 +3,24 @@
     <div class="card-body">
 
       <div class="alert alert-warning" role="alert">
-        <strong>Notice</strong> This section is incomplete!!
+        <h5>Notice</h5>
+        Due to time constraint, this beta version of the application do not have a direct user interface to add and maintain the following data:
+        <br>
+        <ul>
+          <li>Local hotel information</li>
+          <li>Local caterer information</li>
+          <li>Notes for the Do First instructions, Attention notes & WiFi access instructions</li>
+          <li>Application reference data such as email templates, the application’s http port, configurable UI text, view titles and miscellaneous settings.</li>
+        </ul>
+        The above information are kept in a number of configuration data files. All or some of these data files are used to import data into the appliction's database by running batch scripts.
       </div>
 
-      <p>Instructions on how to update data will be posted here. This is a work in progress.</p>
+      <div class="alert alert-warning" role="alert">
+        For more information please refer to the GE Meeting & Event Request App - Maintenance & Upkeep Guide.
+      </div>
+       <div class="alert alert-warning" role="alert">
+         For additional technical support, you can contact the current application development lead (Michelle Nie at 2michellen@gmail.com)
+      </div>
 
     </div>
   </div>
